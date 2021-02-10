80e454cc3becbf7ab31c1a7b7cc5889e127ff59b
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-02-09 18:18:14 -0600

    Created new filter for min price on products View def list

--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -248,6 +248,7 @@ class Products(ViewSet):
         order = self.request.query_params.get('order_by', None)
         direction = self.request.query_params.get('direction', None)
         number_sold = self.request.query_params.get('number_sold', None)
+        min_price = self.request.query_params.get('min_price', None)
 
         if order is not None:
             order_filter = order
@@ -271,6 +272,9 @@ class Products(ViewSet):
                 return False
 
             products = filter(sold_filter, products)
+        
+        if min_price is not None:
+            products = products.filter(price >= min_price)
 
         serializer = ProductSerializer(
             products, many=True, context={'request': request})
