fdaae108a4b0796d62c0a5c96e4111eb3e108c84
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-02-09 21:23:05 -0600

    Changed how the user is loooked up

--- bangazonapi/views/paymenttype.py
+++ bangazonapi/views/paymenttype.py
@@ -81,7 +81,8 @@ class Payments(ViewSet):
         """Handle GET requests to payment type resource"""
         payment_types = Payment.objects.all()
 
-        customer_id = self.request.query_params.get('customer', None)
+        customer = Customer.objects.get(user=request.auth.user)
+        customer_id = customer.user.id
 
         if customer_id is not None:
             payment_types = payment_types.filter(customer__id=customer_id)
